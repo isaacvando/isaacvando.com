cb490a312c17001168867e1c3c7eadf07ce37a82
Isaac Van Doren <69181572+isaacvando@users.noreply.github.com>
2022-07-01 14:45:46 -0500

    changes... descriptive I know

--- src/components/Shopping.js
+++ src/components/Shopping.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import {React, useState, useEffect, createContext} from 'react';
 import LoginButton from './LoginButton';
 import LogoutButton from './LogoutButton';
 import Profile from './Profile';
@@ -6,7 +6,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 import Button from '@mui/material/Button';
 import { initializeApp } from "firebase/app";
 import { } from 'firebase/database';
-import { getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signOut, beforeSignIn} from 'firebase/auth';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -26,11 +26,28 @@ const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 
+export const AuthContext = createContext()
+
+export const AuthContextProvider = props => {
+	const [user, setUser] = useState()
+	const [error, setError] = useState()
+  
+	useEffect(() => {
+	  const unsubscribe = onAuthStateChanged(getAuth(), setUser, setError)
+	  return () => unsubscribe()
+	}, [])
+	return <AuthContext.Provider value={{ user, error }} {...props} />
+  }
+
 onAuthStateChanged(auth, user => {
 	console.log("You are logged in as ", user);
+	console.log(`auth: ${auth}`);
+
+	if(user != null && user.email != "redacted@gmail.com" && user.email != "redacted@gmail.com") {
+		signOut(auth);
+		throw Error("Hold on now... you weren't supposed to try to use this!\nNow I shake my fist at firebase for not implementing a real solution to prevent user sign up. Goodbye.")
+	}
 });
-
-
 
 const Shopping = () => {
 
@@ -38,16 +55,18 @@ const Shopping = () => {
 	return (
 		<>
 		<h1>Shopping</h1>
-		<Button variant="outlined" onClick={() => signIn()}>Log in</Button>
+		<Button variant="outlined" onClick={() => logIn()}>Log in</Button>
+		<Button variant="outlined" onClick={() => signOut(auth)}>Log out</Button>
 		<hr className="Separator"></hr>
 		</>
 	)
 };
 
-const signIn = async () => {
+const logIn = async () => {
 	var result = await signInWithPopup(auth, new GoogleAuthProvider)
 	return result;
 }
+
 
 export default Shopping;
 
