903a57a9566d82c1af65aaff7052203ea0fee09e
Isaac Van Doren <69181572+isaacvando@users.noreply.github.com>
2022-07-02 01:09:53 -0500

    login working properly

--- src/components/Shopping.js
+++ src/components/Shopping.js
@@ -20,22 +20,38 @@ const db = getDatabase(app);
 
 
 // condense usages
+// onAuthStateChanged(auth, user => {
+// 	console.log("You are logged in as ", user);
+// 	if (user != null)
+// 		console.log(user.displayName)
+
+// 	const dbRef = ref(db);
+// 	get(child(dbRef, 'emails')).then((snapshot) => {
+// 	if (snapshot.exists()) {
+// 		// console.log(snapshot.val());
+// 		var emails = snapshot.val();
+// 		if(user != null && !emails.includes(user.email)) {
+// 			signOut(auth);
+// 			throw Error("Hold on now... you weren't supposed to try to use this!\nNow I shake my fist at firebase for not implementing a real solution to prevent user sign up. Goodbye.");
+// 		}
+// 	} else {
+// 		console.log("No data available");
+// 	}
+// 	}).catch((error) => {
+// 		console.error(error);
+// 	});
+// });
+
 onAuthStateChanged(auth, user => {
 	console.log("You are logged in as ", user);
-	if (user != null)
-		console.log(user.displayName)
-
 	const dbRef = ref(db);
 	get(child(dbRef, 'emails')).then((snapshot) => {
 	if (snapshot.exists()) {
-		// console.log(snapshot.val());
 		var emails = snapshot.val();
 		if(user != null && !emails.includes(user.email)) {
 			signOut(auth);
 			throw Error("Hold on now... you weren't supposed to try to use this!\nNow I shake my fist at firebase for not implementing a real solution to prevent user sign up. Goodbye.");
 		}
-	} else {
-		console.log("No data available");
 	}
 	}).catch((error) => {
 		console.error(error);
@@ -44,6 +60,7 @@ onAuthStateChanged(auth, user => {
 
 const Shopping = () => {
 	const [name, setName] = useState(null);
+	const [isAuthenticated, setIsAuthenticated] = useState(false);
 	// useEffect(() => {
 	// 	// if(auth.currentUser != null) {
 	// 	// 	setName(auth.currentUser.displayName);
@@ -59,8 +76,12 @@ const Shopping = () => {
 	// 	}));
 	// },[]);
 
+	
 	onAuthStateChanged(auth, user => {
-		setName(user.displayName);
+		if(user != null && isAuthenticated)
+			setName(user.displayName);
+		else
+			setName(null);
 	});
 
 
@@ -68,16 +89,24 @@ const Shopping = () => {
 		<>
 		<h1>Shopping</h1>
 		{name != null && <p>Hello {name}!</p>}
-		<Button variant="outlined" onClick={() => logIn()}>Log in</Button>
-		<Button variant="outlined" onClick={() => signOut(auth)}>Log out</Button>
+		{ isAuthenticated ?<Button variant="outlined" onClick={() => logOut(setIsAuthenticated)}>Log out</Button>
+		: <Button variant="outlined" onClick={() => logIn(setIsAuthenticated)}>Log in</Button>}
+		
 		<hr className="Separator"></hr>
 		</>
 	)
 };
 
-const logIn = async () => {
+const logIn = async (setIsAuthenticated) => {
 	var result = await signInWithPopup(auth, new GoogleAuthProvider())
+	setIsAuthenticated(true);
 	return result;
+}
+
+const logOut = (setIsAuthenticated) => {
+	// setName(null);
+	signOut(auth);
+	setIsAuthenticated(false);
 }
 
 export default Shopping;
