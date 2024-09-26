fdc1e106eb87d3e511050252b28bc79dec21c7cf
Isaac Van Doren <69181572+isaacvando@users.noreply.github.com>
2024-09-26 18:47:34 -0500

    only upload www dir to pages

--- generate.sh
+++ generate.sh
@@ -2,4 +2,4 @@
 
 name=$1
 
-cat $name.md | cmark | sed 's/<a /<a target="_blank" /g' | ./highlight.js > $name.html
+cat $name.md | cmark | sed 's/<a /<a target="_blank" /g' | ./highlight.js > www/$name.html
